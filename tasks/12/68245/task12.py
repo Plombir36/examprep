4c99a3cb242da6771f8ee47e961f8ed773417fc0
Plombir36 <kirillpetrof.123.kp@gmail.com>
2025-03-18 23:35:15 +0900

    add 12/68245 Python and C++ solution

---
+++ tasks/12/68245/task12.py
@@ -0,0 +1,21 @@
+def isprime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+s = '0'+'4'*31+'03'
+
+for i in range(15):
+    s = s.replace("44", "34", 1)
+    if (isprime(sum(map(int, s)) - 2)):
+        print(s.count('3'))
+        break
+
+for i in range(15):
+    s = s.replace("34", "33", 1)
+    if (isprime(sum(map(int, s)) - 2)):
+        print(s.count('3'))
+        break
